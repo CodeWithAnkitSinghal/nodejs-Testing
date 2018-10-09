c3933784e7386ad1ab1f599907042f79b17b5461
CodeWithAnkitSinghal <singhalankit769@gmail.com>
2018-10-10 02:20:26 +0530

    Using describe function to group the unit test together in logical way

--- server/server.test.js
+++ server/server.test.js
@@ -3,27 +3,35 @@ const expect = require('expect');
 
 const superTest = require('supertest');
 
-it('should return hello world', (done) => {
-    superTest(server)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-        expect(res.body).toInclude({
-            error: 'page not found'
-        });
-    })
-    .end(done);
-});
+describe('Server', () => {
 
-it('should return users list', (done) => {
-    superTest(server)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-        expect(res.body).toInclude({
-            name: 'Ankit',
-            age: 26
+    describe('Get /', () => {
+        it('should return hello world', (done) => {
+            superTest(server)
+            .get('/')
+            .expect(404)
+            .expect((res) => {
+                expect(res.body).toInclude({
+                    error: 'page not found'
+                });
+            })
+            .end(done);
         });
-    })
-    .end(done);
+    });
+
+    describe('Get /Users', () => {
+        it('should return users list', (done) => {
+            superTest(server)
+            .get('/users')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body).toInclude({
+                    name: 'Ankit',
+                    age: 26
+                });
+            })
+            .end(done);
+        });
+    });
+
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,21 +1,55 @@
 const utils = require('./utils');
 const expect = require('expect');
 
-it('should add two numbers', () => {
-    var res = utils.add(2,3);
-    // if(res !== 5){
-    //     throw new Error(`Expected 5 but got ${res}`);
-    // }
-    expect(res).toBe(5).toBeA('number');
-});
 
-it('should square a number', () => {
-    var res = utils.square(3);
-    // if(res !== 9){
-    //     throw new Error(`Expected 9 but got ${res}`);
-    // }
-    expect(res).toBe(9).toBeA('number');
-});
+//grouping the uni test together in logical way
+describe('Utils', () => {
+    describe('#Add', () => {
+        it('should add two numbers', () => {
+            var res = utils.add(2,3);
+            // if(res !== 5){
+            //     throw new Error(`Expected 5 but got ${res}`);
+            // }
+            expect(res).toBe(5).toBeA('number');
+        });
+        it('should async add 2 numbers', (done) => {
+            utils.asyncAdd(2,3, (sum) => {
+                expect(sum).toBe(5).toBeA('number');
+                done();
+            });
+        });
+    });    
+
+    describe('Square', () => {
+        it('should square a number', () => {
+            var res = utils.square(3);
+            // if(res !== 9){
+            //     throw new Error(`Expected 9 but got ${res}`);
+            // }
+            expect(res).toBe(9).toBeA('number');
+        });
+        it('should async square a numbers', (done) => {
+            utils.asyncSquare(3, (sqr) => {
+                expect(sqr).toBe(9).toBeA('number');
+                done();
+            });
+        });
+    });
+    });
+    
+    describe('UserData', () => {
+        it('should verify first and last name are set', () => {
+            var user = {
+                location: 'India',
+                age: 26
+            };
+            var res = utils.setName(user, 'Ankit Singhal');
+            expect(res).toInclude({
+                firstName:'Ankit',
+                lastName:'Singhal'
+            });
+        });
+    });
 
 it('should expect some value', () => {
     //expect(12).toNotBe(13).toNotBeA('string');
@@ -25,29 +59,4 @@ it('should expect some value', () => {
     expect([2,3,4]).toExclude(1);
 });
 
-it('should verify first and last name are set', () => {
-    var user = {
-        location: 'India',
-        age: 26
-    };
-    var res = utils.setName(user, 'Ankit Singhal');
-    expect(res).toInclude({
-        firstName:'Ankit',
-        lastName:'Singhal'
-    });
-});
-
-it('should async add 2 numbers', (done) => {
-    utils.asyncAdd(2,3, (sum) => {
-        expect(sum).toBe(5).toBeA('number');
-        done();
-    });
-});
-
-it('should async square a numbers', (done) => {
-    utils.asyncSquare(3, (sqr) => {
-        expect(sqr).toBe(9).toBeA('number');
-        done();
-    });
-});
 
